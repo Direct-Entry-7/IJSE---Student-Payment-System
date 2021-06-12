2659fe94cd572273f5c04aa713061c217e50393a
Gayal Seneviratne <gayals40@gmail.com>
2021-06-12 20:09:02 +0530

    Maiform  loaded to the stage

---
+++ src/AppInitializer.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent mainForm = FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
+        Scene mainScene = new Scene(mainForm);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Student Payment System");
+        primaryStage.setResizable(true);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+    }
+}
