4883c95204c585644944c93090363d55cbf0da18
Gayal Seneviratne <gayals40@gmail.com>
2021-06-21 20:06:10 +0530

    Course model created

--- src/controller/CourseFormController.java
+++ src/controller/CourseFormController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
 public class CourseFormController {
+    public JFXTextField txtCourseCode;
+    public JFXTextField txtName;
+    public JFXTextField txtCourseFee;
+    public JFXTextField txtDuration;
+    public JFXTextField txtDescription;
+    public JFXButton btnAddCourse;
+    public TableView tblCourses;
+
+    public void btnAddCourse_OnAction(ActionEvent actionEvent) {
+
+    }
 }
---
+++ src/model/Course.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Course {
+    private String courseCode;
+    private String name;
+    private Double courseFee;
+    private String duration;
+    private String description;
+
+    public Course() {
+
+    }
+
+    public Course(String courseCode, String name, Double courseFee, String duration, String description) {
+        this.courseCode = courseCode;
+        this.name = name;
+        this.courseFee = courseFee;
+        this.duration = duration;
+        this.description = description;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCourseFee() {
+        return courseFee;
+    }
+
+    public void setCourseFee(Double courseFee) {
+        this.courseFee = courseFee;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
